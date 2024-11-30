92c437009bc22d9fc4d3b42b17bdd4c98f16e34a
Samuel <chiodisamuel@gmail.com>
2024-11-30 04:10:43 -0300

    add necessary environment config for streamlit and change gitignore

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from htmlTemplates import css, bot_template, user_template
 
 # Load environment variables
 load_dotenv()
-openai.api_key = os.getenv('OPENAI_API_KEY')  # Ensure this line is added to set your API key
+openai.api_key = st.secrets["OPENAI_API_KEY"]  # Ensure this line is added to set your API key
 
 # -------------------------------------------------------------------------------------------------------------------------- Extract data from pdf
 def get_pdf_text(pdf_docs):
